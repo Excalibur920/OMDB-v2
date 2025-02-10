ad45e09121f7de8ab3df82d65721dca22b11be8d
Ethan Belton <48123058+Excalibur920@users.noreply.github.com>
2025-02-10 12:04:48 +0000

    Add files via upload

---
+++ moviesrch.sql
@@ -0,0 +1,111 @@
+/*
+SQLyog Ultimate v11.01 (64 bit)
+MySQL - 5.5.5-10.4.11-MariaDB : Database - moviesrch
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`moviesrch` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+/*Table structure for table `actors` */
+
+DROP TABLE IF EXISTS `actors`;
+
+CREATE TABLE `actors` (
+  `actor_id` int(11) NOT NULL AUTO_INCREMENT,
+  `actor_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`actor_id`),
+  UNIQUE KEY `actor_name` (`actor_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4;
+
+/*Table structure for table `genres` */
+
+DROP TABLE IF EXISTS `genres`;
+
+CREATE TABLE `genres` (
+  `genre_id` int(11) NOT NULL AUTO_INCREMENT,
+  `genre_name` varchar(100) NOT NULL,
+  PRIMARY KEY (`genre_id`),
+  UNIQUE KEY `genre_name` (`genre_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4;
+
+/*Table structure for table `movieactors` */
+
+DROP TABLE IF EXISTS `movieactors`;
+
+CREATE TABLE `movieactors` (
+  `movie_id` int(11) NOT NULL,
+  `actor_id` int(11) NOT NULL,
+  PRIMARY KEY (`movie_id`,`actor_id`),
+  KEY `actor_id` (`actor_id`),
+  CONSTRAINT `movieactors_ibfk_1` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`movie_id`),
+  CONSTRAINT `movieactors_ibfk_2` FOREIGN KEY (`actor_id`) REFERENCES `actors` (`actor_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Table structure for table `moviegenres` */
+
+DROP TABLE IF EXISTS `moviegenres`;
+
+CREATE TABLE `moviegenres` (
+  `movie_id` int(11) NOT NULL,
+  `genre_id` int(11) NOT NULL,
+  PRIMARY KEY (`movie_id`,`genre_id`),
+  KEY `genre_id` (`genre_id`),
+  CONSTRAINT `moviegenres_ibfk_1` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`movie_id`),
+  CONSTRAINT `moviegenres_ibfk_2` FOREIGN KEY (`genre_id`) REFERENCES `genres` (`genre_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Table structure for table `movies` */
+
+DROP TABLE IF EXISTS `movies`;
+
+CREATE TABLE `movies` (
+  `movie_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `year` int(11) DEFAULT NULL CHECK (`year` > 1800),
+  `rated` varchar(50) DEFAULT NULL,
+  `released` varchar(50) DEFAULT NULL,
+  `runtime` varchar(50) DEFAULT NULL,
+  `plot` text DEFAULT NULL,
+  `language` varchar(255) DEFAULT NULL,
+  `poster_url` text DEFAULT NULL,
+  `imdb_rating` decimal(3,1) DEFAULT NULL,
+  PRIMARY KEY (`movie_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
+
+/*Table structure for table `searches` */
+
+DROP TABLE IF EXISTS `searches`;
+
+CREATE TABLE `searches` (
+  `search_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `prompt` text NOT NULL,
+  `search_time` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`search_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `searches_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=64 DEFAULT CHARSET=utf8mb4;
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
